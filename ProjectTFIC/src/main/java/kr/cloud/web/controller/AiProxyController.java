7fe579bba5a34812938e22359f9e71f687ca877e
기융 <ert1007@naver.com>
2025-08-05 00:08:28 +0900

    Merge branch 'main' of https://github.com/projectTFIC/projectTFIC.git into main

--- ProjectTFIC/src/main/java/kr/cloud/web/controller/AiProxyController.java
+++ ProjectTFIC/src/main/java/kr/cloud/web/controller/AiProxyController.java
@@ -20,7 +20,7 @@ import lombok.RequiredArgsConstructor;
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
-@RequestMapping("/aiapi")
+@RequestMapping("/proxy")
 @RequiredArgsConstructor
 public class AiProxyController {
 	
