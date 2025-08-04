d90d12d114ef3c31ee34fbd3f710a83c09cd311f
hbjoo96 <hbjoo9606@gmail.com>
2025-08-04 18:29:27 +0900

    20250804 안전장비 착용여부 게시판 기능

--- ProjectTFIC/src/main/java/kr/cloud/web/controller/AiApiController.java
+++ ProjectTFIC/src/main/java/kr/cloud/web/controller/AiApiController.java
@@ -14,6 +14,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import kr.cloud.web.entity.HeRecordDto;
 import kr.cloud.web.entity.HeRecordViewDto;
+import kr.cloud.web.service.PpeRecordService;
+import kr.cloud.web.entity.PpeRecordDto;
+import kr.cloud.web.entity.PpeRecordViewDto;
 import kr.cloud.web.service.HeRecordService;
 import lombok.RequiredArgsConstructor;
 
@@ -27,8 +30,62 @@ public class AiApiController {
 	// AI 와 API 를 활용한 기능과 관련된 컨트롤러
 	
 	// ㅇ AI / API 중장비 기능 처리 객체
-	private final HeRecordService heRecordService;
+	private final PpeRecordService heRecordService;
+	private final HeRecordService ppeRecordService;
 	
+	// =================================================================
+	// 안전장비 출입 (Personal Protect Equipment)
+	// =================================================================
+	
+	// [ 안전장비 착용여부 : 객체 탐지 기능 요청 ]
+    @PostMapping("/ppe-records")
+    public ResponseEntity<String> createPpeRecord(@RequestBody PpeRecordDto ppeRecordDto) {
+        
+    	try {
+        	// 1. 안전장비 기능 처리 객체를 사용하여 기록저장
+            ppeRecordService.saveRecord(ppeRecordDto);
+            
+            // 2. 성공적으로 완료된 경우, python 에게 성공 신호와 메시지 전송
+            return ResponseEntity.status(HttpStatus.CREATED).body("기록이 성공적으로 생성되었습니다.");
+            
+        } catch (Exception e) {
+        	
+        	// 3. 에러가 발생한 경우, 에러 내용을 출력 + python 에게 에러 신호와 메시지 전송
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("기록 생성 중 오류 발생: "+ e.getMessage());
+            
+        }
+    }
+	
+    
+    // [ 안전장비 착용여부 : 객체 탐지 전체 기록 조회 ]
+    @GetMapping("/ppe-records")
+    public ResponseEntity<List<PpeRecordViewDto>> getAllPpeRecords() {
+    	
+        // 1. 안전장비 착용여부와 관련된 모든 기록 가져오기
+        List<PpeRecordViewDto> records = PpeRecordService.selectAllHeRecords();
+        
+        // 2. 기록 리스트를 성공적으로 가져온 경우, 성공 신호 전달
+        return ResponseEntity.ok(records);
+        
+    }
+
+    
+    // [ 안전장비 착용여부 : 객체 탐지 전체 기록 조회 ]
+    @GetMapping("/ppe-records/{id}")
+    public ResponseEntity<PpeRecordViewDto> getPpeRecordById(@PathVariable int recordId) {
+
+    	// 1. 안전장비 착용여부와 관련된 특정 기록 가져오기
+    	PpeRecordViewDto record = ppeRecordService.selectHeRecordById(recordId);
+        
+        // 2. 해당 기록을 성공적으로 가져온 경우, 성공 신호 전달
+        return ResponseEntity.ok(record);
+        
+    }
+
+    
+	// =================================================================
+	// 중장비 출입 (Heavy Equipment)
+	// =================================================================
 	
 	// [ 중장비 출입 : 객체 탐지 기능 요청 ]
     @PostMapping("/he-records")
@@ -39,13 +96,11 @@ public class AiApiController {
             heRecordService.saveHeRecord(heRecordDto);
 
             // 2. 성공적으로 완료된 경우, python 에게 성공 신호와 메시지 전송
-            return ResponseEntity.status(HttpStatus.CREATED).body("Record created successfully.");
+            return ResponseEntity.status(HttpStatus.CREATED).body("기록이 성공적으로 생성되었습니다.");
 
         } catch (Exception e) {
-            // 3. 에러가 발생한 경우, 에러 내용을 출력 + python 에게 에러 신호와 메시지 전송
-            System.out.println("Error creating record: " + e.getMessage());
-            
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error creating record: " + e.getMessage());
+            // 3. 에러가 발생한 경우, 에러 내용을 출력 + python 에게 에러 신호와 메시지 전송            
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("기록 생성 중 오류 발생: "  + e.getMessage());
             
         }
     	
