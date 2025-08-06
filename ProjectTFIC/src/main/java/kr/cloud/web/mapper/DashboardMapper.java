ab74e7cd564d1151b3aedf3f69d106b42dfcb273
조명현 <0124cmh@naver.com>
2025-08-06 17:41:23 +0900

    대시보드 로그 기록 매퍼 수정

--- ProjectTFIC/src/main/java/kr/cloud/web/mapper/DashboardMapper.java
+++ ProjectTFIC/src/main/java/kr/cloud/web/mapper/DashboardMapper.java
@@ -39,12 +39,13 @@ public interface DashboardMapper {
 			  '사고 감지' AS label,
 			  '객체 감지 시스템에서 새로운 이벤트가 감지되었습니다' AS message,
 			  t.location,
-			  d.device_name AS name,                      -- 카메라명
-			  a.record_title AS chipValue,               -- ⬅️ 사고 감지: record_title
+			  d.device_name AS name,
+			  a.record_title AS chipValue,
+			  t.reg_date,                               -- 원본 날짜 컬럼 추가
 			  DATE_FORMAT(t.reg_date, '%p %I:%i') AS time
 			FROM acc_record a
 			JOIN type_info t ON a.type_id = t.type_id
-			JOIN devices d   ON t.device_id = d.device_id
+			JOIN devices d ON t.device_id = d.device_id
 
 			UNION ALL
 
@@ -54,11 +55,12 @@ public interface DashboardMapper {
 			  '객체 감지 시스템에서 새로운 이벤트가 감지되었습니다',
 			  t.location,
 			  d.device_name,
-			  p.record_title,                             -- ⬅️ 미착용 감지: record_title
+			  p.record_title,
+			  t.reg_date,
 			  DATE_FORMAT(t.reg_date, '%p %I:%i')
 			FROM ppe_record p
 			JOIN type_info t ON p.type_id = t.type_id
-			JOIN devices d   ON t.device_id = d.device_id
+			JOIN devices d ON t.device_id = d.device_id
 
 			UNION ALL
 
@@ -68,15 +70,17 @@ public interface DashboardMapper {
 			  '객체 감지 시스템에서 새로운 이벤트가 감지되었습니다',
 			  t.location,
 			  d.device_name,
-			  h.he_number,                                -- ⬅️ 입출입 감지: he_number
+			  h.he_number,
+			  t.reg_date,
 			  DATE_FORMAT(t.reg_date, '%p %I:%i')
 			FROM he_record h
 			JOIN type_info t ON h.type_id = t.type_id
-			JOIN devices d   ON t.device_id = d.device_id
+			JOIN devices d ON t.device_id = d.device_id
 
-			ORDER BY time DESC
+			ORDER BY reg_date DESC
 			LIMIT 10
-						""")
+
+									""")
 	List<LogHistoryItemDto> selectLatestLogs();
 
 }
